598e2d37484b787773ef348107666315e77f5d87
Void <jnxkcxkjjcjkccm@gmail.com>
2026-03-16 07:57:29 -0400

    Create guess_number.py

---
+++ games/guess_number.py
@@ -0,0 +1,12 @@
+import random
+
+print("🎮 Guess The Number Game")
+
+number = random.randint(1, 10)
+
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == number:
+    print("🎉 Correct! You win!")
+else:
+    print("❌ Wrong! The number was", number)
