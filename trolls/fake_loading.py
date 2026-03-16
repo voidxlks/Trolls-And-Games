9b4a02a6d9f1de3679a15444f8691bcd7b9df648
Void <jnxkcxkjjcjkccm@gmail.com>
2026-03-16 07:58:02 -0400

    Create fake_loading.py

---
+++ trolls/fake_loading.py
@@ -0,0 +1,11 @@
+import time
+
+print("Initializing system...")
+
+for i in range(101):
+    print(f"Loading... {i}%")
+    time.sleep(0.05)
+
+print("System hacked! 😈")
+time.sleep(1)
+print("Just kidding 😂")
